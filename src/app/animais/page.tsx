007b4d20b9ef40624939854ea48dce348e8324ac
pedro henrique <pedro.henrike2812@gmail.com>
2024-06-04 17:32:32 -0300

    wip: criando grid de animais em tela

--- src/app/animais/page.tsx
+++ src/app/animais/page.tsx
@@ -2,10 +2,10 @@ import Image from 'next/image'
 
 export default async function AnimalsPage() {
   return (
-    <section className="flex">
-      <nav className="flex flex-col min-h-full w-[392px]">
+    <section className="h-screen w-screen flex">
+      <nav className="flex flex-col h-full w-[392px]">
         <header className="bg-red-400 flex items-center py-8">
-          <div className="flex flex-col gap-7  mx-auto">
+          <div className="flex flex-col gap-7 mx-auto">
             <Image
               src="/logo.svg"
               alt="Logo da aplicação"
@@ -17,7 +17,7 @@ export default async function AnimalsPage() {
                 <select
                   name="state"
                   id="state"
-                  className=" text-white outline-none font-bold bg-red-400"
+                  className="text-white outline-none font-bold bg-red-400"
                 >
                   <option value="PE">MA</option>
                 </select>
@@ -27,7 +27,7 @@ export default async function AnimalsPage() {
                 <select
                   name="city"
                   id="city"
-                  className="bg-red-400 text-white text-center outline-none border-none font-bold "
+                  className="bg-red-400 text-white text-center outline-none border-none font-bold"
                 >
                   <option value="recife">São Luís</option>
                 </select>
@@ -45,7 +45,7 @@ export default async function AnimalsPage() {
           </div>
         </header>
 
-        <div className="flex flex-col bg-red-300 py-5 pl-14 pr-10">
+        <div className="flex flex-col bg-red-300 py-5 pl-14 pr-10 flex-grow">
           <h2 className="font-extrabold text-xl text-white pb-4">Filtros</h2>
 
           <form action="" className="w-full flex flex-col gap-5">
@@ -111,6 +111,29 @@ export default async function AnimalsPage() {
           </form>
         </div>
       </nav>
+
+      <main className="flex-grow">
+        <header className="flex justify-between w-full">
+          <p>Encontre 324 amigos na sua cidade</p>
+
+          <select name="filter" id="filter">
+            <option value="gato-e-cachorro">Gatos e Cachorros</option>
+          </select>
+        </header>
+
+        <div className="grid  grid-cols-3">
+          <div className="w-[280px] h-[280px] bg-[#0D3B66]">
+            <header className="bg-orange-500">
+              <Image
+                src="/images/dog.png"
+                width={200}
+                height={200}
+                alt="Imagem de cachorro"
+              />
+            </header>
+          </div>
+        </div>
+      </main>
     </section>
   )
 }
