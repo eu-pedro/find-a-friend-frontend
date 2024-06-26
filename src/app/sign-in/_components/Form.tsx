30446b3efc030f393174a5608d953e41e39be9a2
Pedro Henrique <pedro.henrike2812@gmail.com>
2024-06-26 20:23:28 -0300

    feat: add password visible and change type input

--- src/app/sign-in/_components/Form.tsx
+++ src/app/sign-in/_components/Form.tsx
@@ -1,10 +1,17 @@
+'use client'
+
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
+import { Eye, EyeOff } from 'lucide-react'
+import { useState } from 'react'
 
 export function Form() {
+  const [isVisible, setIsVisible] = useState(false)
+
+  console.log(isVisible)
   return (
     <form className="w-[488px] h-full flex flex-col justify-around">
-      <div className="">
+      <div>
         <fieldset className="flex flex-col gap-2">
           <label htmlFor="email" className="text-blue-bold text-base font-bold">
             Email
@@ -17,7 +24,7 @@ export function Form() {
           />
         </fieldset>
 
-        <fieldset className="flex flex-col gap-2 mt-4">
+        <fieldset className="flex flex-col gap-2 mt-4 relative">
           <label
             htmlFor="password"
             className="text-blue-bold text-base font-bold"
@@ -25,11 +32,22 @@ export function Form() {
             Senha
           </label>
           <Input
-            type="password"
+            type={isVisible ? 'text' : 'password'}
             id="password"
             placeholder="************"
             className="bg-light-gray border-2 border-light-gray text-blue-bold font-semibold placeholder:bold placeholder:text-blue-bold py-6"
           />
+          {isVisible ? (
+            <Eye
+              className="absolute right-5 top-11 hover:cursor-pointer"
+              onClick={() => setIsVisible((state) => !state)}
+            />
+          ) : (
+            <EyeOff
+              className="absolute right-5 top-11 hover:cursor-pointer"
+              onClick={() => setIsVisible((state) => !state)}
+            />
+          )}
         </fieldset>
       </div>
 
