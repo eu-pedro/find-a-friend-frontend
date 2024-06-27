4964aa8827c9b30c06655ebeae8d350e16417fa5
Pedro Henrique <pedro.henrike2812@gmail.com>
2024-06-26 21:40:16 -0300

    feat: creating validation form with zod and hookform

--- src/app/sign-in/_components/Form.tsx
+++ src/app/sign-in/_components/Form.tsx
@@ -2,13 +2,74 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Eye, EyeOff } from 'lucide-react'
 import { useState } from 'react'
+import * as z from 'zod'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const createUserFormSchema = z
+  .object({
+    name: z.string().min(1, {
+      message: 'Preencha todos os campos!',
+    }),
+    email: z
+      .string()
+      .min(1, {
+        message: 'Preencha todos os campos!',
+      })
+      .email('Formato de e-mail inválido!')
+      .min(1, {
+        message: 'Preencha todos os campos!',
+      }),
+    password: z
+      .string()
+      .min(1, {
+        message: 'Preencha todos os campos!',
+      })
+      .max(6, {
+        message: 'Senha deve ter no máximo 6 caracteres!',
+      }),
+    confirm_password: z
+      .string()
+      .min(1, {
+        message: 'Preencha todos os campos!',
+      })
+      .max(6, {
+        message: 'Senha deve ter no máximo 6 caracteres!',
+      }),
+  })
+  .refine(({ password, confirm_password }) => password === confirm_password, {
+    message: 'As senhas devem ser iguais!',
+    path: ['confirm_password'],
+  })
+
+type CreateUserFormData = z.infer<typeof createUserFormSchema>
 
 export function Form() {
   const [isVisible, setIsVisible] = useState(false)
 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    clearErrors,
+    reset,
+  } = useForm<CreateUserFormData>({
+    resolver: zodResolver(createUserFormSchema),
+    mode: 'all',
+  })
+
+  function handleSubmitForm(data: CreateUserFormData) {
+    alert('usuário criado com sucesso' + JSON.stringify(data))
+    clearErrors()
+    reset()
+  }
+
   return (
-    <form className="w-[488px] h-full flex flex-col justify-around">
-      <div className="flex flex-col gap-3">
+    <form
+      onSubmit={handleSubmit(handleSubmitForm)}
+      className="w-[488px] h-full flex flex-col justify-around"
+    >
+      <div className="flex flex-col gap-2">
         <fieldset className="flex flex-col gap-2">
           <label htmlFor="name" className="text-blue-bold text-base font-bold">
             Nome
@@ -18,7 +79,11 @@ export function Form() {
             id="name"
             placeholder="Digite seu nome"
             className="bg-light-gray border-2 border-light-gray text-blue-bold font-semibold placeholder:bold placeholder:text-blue-bold py-6"
+            {...register('name')}
           />
+          {errors.name && (
+            <span className="text-red-500 text-xs">{errors.name.message}</span>
+          )}
         </fieldset>
         <fieldset className="flex flex-col gap-2">
           <label htmlFor="email" className="text-blue-bold text-base font-bold">
@@ -29,7 +94,11 @@ export function Form() {
             id="email"
             placeholder="nome@email.com"
             className="bg-light-gray border-2 border-light-gray text-blue-bold font-semibold placeholder:bold placeholder:text-blue-bold py-6"
+            {...register('email')}
           />
+          {errors.email && (
+            <span className="text-red-500 text-xs">{errors.email.message}</span>
+          )}
         </fieldset>
 
         <fieldset className="flex flex-col gap-2 relative">
@@ -44,6 +113,7 @@ export function Form() {
             id="password"
             placeholder="************"
             className="bg-light-gray border-2 border-light-gray text-blue-bold font-semibold placeholder:bold placeholder:text-blue-bold py-6"
+            {...register('password')}
           />
           {isVisible ? (
             <Eye
@@ -55,6 +125,11 @@ export function Form() {
               className="absolute right-5 top-11 hover:cursor-pointer"
               onClick={() => setIsVisible((state) => !state)}
             />
+          )}
+          {errors.password && (
+            <span className="text-red-500 text-xs">
+              {errors.password.message}
+            </span>
           )}
         </fieldset>
 
@@ -70,6 +145,7 @@ export function Form() {
             id="confirm_password"
             placeholder="************"
             className="bg-light-gray border-2 border-light-gray text-blue-bold font-semibold placeholder:bold placeholder:text-blue-bold py-6"
+            {...register('confirm_password')}
           />
           {isVisible ? (
             <Eye
@@ -81,6 +157,11 @@ export function Form() {
               className="absolute right-5 top-11 hover:cursor-pointer"
               onClick={() => setIsVisible((state) => !state)}
             />
+          )}
+          {errors.confirm_password && (
+            <span className="text-red-500 text-xs">
+              {errors.confirm_password.message}
+            </span>
           )}
         </fieldset>
       </div>
