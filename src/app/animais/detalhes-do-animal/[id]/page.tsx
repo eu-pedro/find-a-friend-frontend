8f332dabdf32112d847454df3dce2767a5a3bb03
pedro henrique <pedro.henrike2812@gmail.com>
2025-01-10 18:15:55 -0300

    style: ensure more semantics for AnimalsDetailsPage

--- src/app/animais/detalhes-do-animal/[id]/page.tsx
+++ src/app/animais/detalhes-do-animal/[id]/page.tsx
@@ -8,20 +8,20 @@ export default function AnimalDetailsPage() {
   const { back } = useRouter()
 
   return (
-    <section className="h-full w-full flex gap-80 items-stretch  bg-pink-100">
+    <section className="min-h-screen w-full flex gap-80 items-stretch bg-pink-100">
       <aside className="fixed min-h-[620px] w-fit bg-red-light flex flex-col justify-between py-8 px-6">
         <Image src="/logo.svg" width={45} height={46.67} alt="" />
 
         <button
-          className="bg-yellow-bold p-3 rounded-2xl"
+          className="bg-yellow-bold p-3 rounded-2xl hover:cursor-pointer"
           onClick={() => back()}
         >
           <ArrowLeft color="#0D3B66" className="font-extrabold" size={20} />
         </button>
       </aside>
 
-      <div className="ml-80 w-[600px] bg-white border-2 border-[#D3E2E5] rounded-3xl">
-        <header className="bg-zinc-300 flex justify-center w-full h-[330px]">
+      <div className="ml-80 mt-16 w-[704px] bg-white border-none rounded-3xl">
+        <header className="bg-slate-300 flex justify-center w-full h-[330px] rounded-t-3xl">
           <Image
             src="/dog.png"
             alt=""
@@ -43,21 +43,29 @@ export default function AnimalDetailsPage() {
             {/* {
               COMPONENTIZAR.
             } */}
-            <div className="flex flex-col gap-4 p-6 border-[#E7EBF0] border-2 rounded-3xl">
+            <div className="w-full flex flex-col gap-4 p-6 border-[#E7EBF0] border-2 rounded-3xl">
               <div className="flex items-center gap-2">
                 <Zap size={18} className="" />
+                <Zap size={18} className="" />
+                <Zap size={18} className="" />
+                <Zap size={18} className="" />
+                <Zap size={18} className="opacity-10" />
               </div>
-              <h4 className="leading-tight text-nowrap">Muita energia</h4>
+              <h4 className="leading-tight text-nowrap text-blue-bold font-semibold text-lg">
+                Muita energia
+              </h4>
             </div>
 
-            <div className="flex flex-col gap-4 p-6 border-[#E7EBF0] border-2 rounded-3xl">
+            <div className="w-full flex flex-col gap-4 p-6 border-[#E7EBF0] border-2 rounded-3xl">
               <div className="flex items-center gap-2">
                 <Scan size={18} className="" />
               </div>
-              <h4 className="leading-tight text-nowrap">Ambiente amplo</h4>
+              <h4 className="leading-tight text-nowrap text-blue-bold font-semibold text-lg">
+                Ambiente amplo
+              </h4>
             </div>
 
-            <div className="flex flex-col gap-4 p-6 border-[#E7EBF0] border-2 rounded-3xl">
+            <div className="w-full flex flex-col gap-4 p-6 border-[#E7EBF0] border-2 rounded-3xl">
               <div className="flex items-center gap-2">
                 <Image src="/full-dot.svg" alt="" width={12} height={12} />
                 <Image
@@ -73,19 +81,23 @@ export default function AnimalDetailsPage() {
                   height={12}
                 />
               </div>
-              <h4 className="leading-tight text-nowrap">Muita energia</h4>
+              <h4 className="leading-tight text-nowrap text-blue-bold font-semibold text-lg">
+                Pequenino
+              </h4>
             </div>
           </div>
 
-          <div className="w-full h-0.5 bg-[#D3E2E5]" />
+          <div className="w-full h-0.5 bg-[#D3E2E5] my-14" />
 
-          <h3>Requesitos para adoção</h3>
+          <h3 className="mb-14 font-bold text-blue-bold text-3xl	">
+            Requesitos para adoção
+          </h3>
 
           <div className="flex flex-col gap-2">
-            <div className="flex gap-3 w-full border-2 border-red-light  py-3 px-8 rounded-xl bg-gradient-to-r from-[#FEEFEF] to-white">
-              <CircleAlert className="text-red-light" />
+            <div className="flex items-center gap-3 w-full border-2 border-red-light  py-3 px-8 rounded-xl bg-gradient-to-r from-[#FEEFEF] to-white">
+              <CircleAlert className="text-red-light" size={20} />
 
-              <span className="text-red-light border-red-light">
+              <span className="text-red-light border-red-light font-semibold text-lg">
                 Local grande para o animal correr e brincar.
               </span>
             </div>
@@ -111,9 +123,9 @@ export default function AnimalDetailsPage() {
               </span>
             </div>
 
-            <div className="w-full h-0.5 bg-[#D3E2E5]" />
+            <div className="w-full h-0.5 bg-[#D3E2E5] my-14" />
 
-            <button className="w-full bg-green-light flex items-center justify-center gap-4 font-extrabold text-white py-4 rounded-3xl">
+            <button className="w-full text-lg bg-green-light flex items-center justify-center gap-4 font-extrabold text-white py-4 rounded-3xl">
               <Image
                 src="/whatsapp.svg"
                 alt="Whatsapp icon"
