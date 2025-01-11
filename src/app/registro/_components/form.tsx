6b5512c3b4bea2009a358dbe50716e96ec863b7e
pedro henrique <pedro.henrike2812@gmail.com>
2025-01-11 17:11:22 -0300

    feat: add the RegisterPage

--- src/app/registro/_components/form.tsx
+++ src/app/registro/_components/form.tsx
@@ -1,14 +1,17 @@
 'use client'
 
+import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Eye, EyeOff } from 'lucide-react'
 import { useState } from 'react'
 
 export function Form() {
-  const [isVisible, setIsVisible] = useState(false)
+  const [password, setPassword] = useState(false)
+  const [confirmPassword, setConfirmPassword] = useState(false)
+
   return (
-    <form className="w-[488px] h-full flex flex-col">
-      <div>
+    <form className="w-[488px] h-full flex flex-col justify-around">
+      <div className="">
         <fieldset className="flex flex-col gap-2">
           <label htmlFor="email" className="text-blue-bold text-base font-bold">
             Email
@@ -29,20 +32,20 @@ export function Form() {
             Senha
           </label>
           <Input
-            type={isVisible ? 'text' : 'password'}
+            type={password ? 'text' : 'password'}
             id="password"
             placeholder="************"
             className="bg-light-gray border-2 border-light-gray text-blue-bold font-semibold placeholder:bold placeholder:text-blue-bold py-6"
           />
-          {isVisible ? (
+          {password ? (
             <Eye
               className="absolute right-5 top-11 hover:cursor-pointer"
-              onClick={() => setIsVisible((state) => !state)}
+              onClick={() => setPassword((state) => !state)}
             />
           ) : (
             <EyeOff
               className="absolute right-5 top-11 hover:cursor-pointer"
-              onClick={() => setIsVisible((state) => !state)}
+              onClick={() => setPassword((state) => !state)}
             />
           )}
         </fieldset>
@@ -55,24 +58,31 @@ export function Form() {
             Senha
           </label>
           <Input
-            type={isVisible ? 'text' : 'password'}
+            type={confirmPassword ? 'text' : 'password'}
             id="confirmPassword"
             placeholder="************"
             className="bg-light-gray border-2 border-light-gray text-blue-bold font-semibold placeholder:bold placeholder:text-blue-bold py-6"
           />
-          {isVisible ? (
+          {confirmPassword ? (
             <Eye
               className="absolute right-5 top-11 hover:cursor-pointer"
-              onClick={() => setIsVisible((state) => !state)}
+              onClick={() => setConfirmPassword((state) => !state)}
             />
           ) : (
             <EyeOff
               className="absolute right-5 top-11 hover:cursor-pointer"
-              onClick={() => setIsVisible((state) => !state)}
+              onClick={() => setConfirmPassword((state) => !state)}
             />
           )}
         </fieldset>
       </div>
+
+      <Button
+        type="submit"
+        className="w-full py-7 bg-blue-bold rounded-3xl text-white font-extrabold text-base"
+      >
+        Cadastrar
+      </Button>
     </form>
   )
 }
